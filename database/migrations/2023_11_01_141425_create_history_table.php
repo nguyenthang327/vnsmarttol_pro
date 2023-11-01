f4cccb4975860b0412236f61988207a07dfbc3f0
nguyenthang327 <nguyenducthang327@gmail.com>
2023-11-01 22:01:51 +0700

    payment card

---
+++ database/migrations/2023_11_01_141425_create_history_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHistoryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('history', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id')->comment('Mã người dùng');
+            $table->text('type')->nullable()->comment('Loại giao dịch: Like nhanh, Card, MB Bank');
+            $table->unsignedBigInteger('coin_first')->comment('Số tiền trước giao dịch');
+            $table->unsignedBigInteger('coin_second')->comment('Số tiền sau giao dịch');
+            $table->unsignedBigInteger('coin')->comment('Số tiền giao dịch');
+            $table->text('code_order')->comment('Mã code giao giao dịch');
+            $table->string('identity_website')->comment('Mã định danh website');
+            $table->text('note')->nullable()->comment('Ghi chú, nội dung giao dịch');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('histories');
+    }
+}
---
+++ database/migrations/2023_11_01_142613_create_card_recharge_history_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCardRechargeHistoryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('card_recharge_history', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id')->comment('Mã người dùng');
+            $table->unsignedInteger('type')->comment('Loại thẻ: 1 => Viettel, 2 => Mobifone, 3 => Vinaphone, 4 => Vietnamobile)');
+            $table->unsignedBigInteger('denomination_money')->comment('Mệnh giá thẻ nạp');
+            $table->unsignedBigInteger('actually_received')->comment('Thực nhận');
+            $table->string('seri', 100)->comment('Số seri');
+            $table->string('id_card', 100)->comment('Mã thẻ');
+            $table->text('trans')->comment('Mã giao dịch');
+            $table->unsignedTinyInteger('status')->comment('Trạng thái giao dịch');
+            $table->string('identity_website')->comment('Mã định danh website');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('card_recharge_history');
+    }
+}
