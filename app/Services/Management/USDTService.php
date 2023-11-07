671fcd0b584302f3a54a697d6ff1d4fe1739f968
nguyenthang327 <nguyenducthang327@gmail.com>
2023-11-07 23:36:52 +0700

    usdt

--- app/Services/Management/USDTService.php
+++ app/Services/Management/USDTService.php
@@ -3,8 +3,10 @@
 namespace App\Services\Management;
 
 use App\Models\RechargeUSDTHistory;
+use App\Models\Setting;
 use Exception;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
 
 class USDTService
@@ -18,11 +20,18 @@ class USDTService
                 'username' => $user->username,
                 'type' => 'fpayment.co',
                 'usdt_number' => $request->usdt_number,
+                'name' => "Tài khoản $user->username nạp usdt",
+                'description' => "Tài khoản $user->username nạp usdt"
             ];
 
             $data = RechargeUSDTHistory::create($params);
 
-            $result = json_decode(Http::get("https://api.web2m.com/$path/$password/$sotaikhoan/$token"));
+            $setting = Setting::first();
+
+            $urlCallback = '';
+            $urlReturn = '';
+
+            $result = json_decode(Http::get("https://fpayment.co/api/AddInvoice.php?address_wallet=$setting->usdt_address_wallet&token_wallet=$setting->usdt_token_wallet&name=$data->name&description=$data->name&amount=$data->usdt_number&request_id=$data->id&callback=$urlCallback&return_url=$urlReturn"));
             
             DB::commit();
             return $data;
